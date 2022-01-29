1785f675af13f4bbde8b0b08cc4b50a0f43e9b42
ssogong <niha130@naver.com>
2022-01-29 17:11:34 +0900

    Modify search input - autofocus, onclick clear value

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -4,9 +4,10 @@ class SearchInput {
   constructor({ $target, onSearch }) {
     const $searchInput = document.createElement("input");
     this.$searchInput = $searchInput;
-    this.$searchInput.placeholder = "고양이를 검색해보세요.|";
+    this.$searchInput.placeholder = "고양이를 검색해보세요.";
 
     $searchInput.className = "SearchInput";
+    $searchInput.autofocus = true;
     $target.appendChild($searchInput);
 
     $searchInput.addEventListener("keyup", e => {
@@ -14,6 +15,10 @@ class SearchInput {
         onSearch(e.target.value);
       }
     });
+
+    $searchInput.addEventListener("click", e => {
+      $searchInput.value = '';
+    })
 
     console.log("SearchInput created.", this);
   }
